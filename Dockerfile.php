db2d98c4af353397edd41a453231b2ff98683285
farahbourmech <bourmechfarah3@gmail.com>
2024-04-12 17:54:54 +0100

    init

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,22 +1,12 @@
 FROM php:apache
 
 RUN echo 'ServerName chromerce-www-1' >> /etc/apache2/apache2.conf
-
-RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    libzip-dev \
-    && apt-get clean && rm -rf /var/lib/apt/lists/* \
-    && docker-php-ext-install mysqli pdo_mysql \
-    && a2enmod rewrite
-
+RUN apt-get update && docker-php-ext-install mysqli pdo_mysql 
 COPY . /var/www/html
-
 ENV DB_HOST=db
 ENV DB_PORT=3306
 ENV DB_USER=chromerce
 ENV DB_PASSWORD=password
 ENV DB_NAME=chromerce
-
 EXPOSE 80
-
 CMD ["apache2-foreground"]
