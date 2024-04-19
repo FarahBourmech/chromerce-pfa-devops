ed7f02cb0e3bd82792b00cca82d1c96d5bdea392
Farah BOURMECH <136247523+FarahBourmech@users.noreply.github.com>
2024-04-19 18:52:16 +0100

    about.php

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>About Chromerce</title>
+	<title>About Chromerce</title> 
 	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
 	<link rel="icon" type="image/png" href="images/favicon.png"/>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -105,7 +105,7 @@
 	}, true);
 	</script>
 
-	<!---------- content ---------->
+	<!---------- content  farah ---------->
 	<div class="content">
 		<div class="container">
 			    <div class="hero">
