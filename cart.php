6cfb70a98d7cfb079b01c0bd6648b38a81552738
Farah BOURMECH <136247523+FarahBourmech@users.noreply.github.com>
2024-04-19 18:30:58 +0100

    cart.php

--- cart.php
+++ cart.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION["loggedin"])) {
   header("location: login.php");
   exit();
 }
-//test 
+//test farah farah aaaaaaaaaaaaaaaaaaaaaaaaaaa
 if (isset($_GET['clear'])) {
   // Clear the cart
   unset($_SESSION['cart_item']);
